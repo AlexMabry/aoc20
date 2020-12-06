b5171f2dfd216924d2509ceb2ffea9a6e84bf468
Alex Mabry <alex.mabry@synapse-wireless.com>
2020-12-06 12:58:42 -0600

    Day 5

--- day05/d5a.py
+++ day05/d5a.py
@@ -3,49 +3,23 @@ from util import decode_list
 
 def read_seats(filename):
     lines = [n for n in open(filename).read().splitlines()]
-
     template = r'(?P<row>[FB]{7})(?P<col>[LR]{3})'
     return decode_list(lines, template)
 
 
-def row_to_int(row):
-    ret = 0
-    if row[0] == 'B':
-        ret += 64
-    if row[1] == 'B':
-        ret += 32
-    if row[2] == 'B':
-        ret += 16
-    if row[3] == 'B':
-        ret += 8
-    if row[4] == 'B':
-        ret += 4
-    if row[5] == 'B':
-        ret += 2
-    if row[6] == 'B':
-        ret += 1
-
-    return ret
+def get_seat_id(seat):
+    row = int(seat.row.replace('B', '1').replace('F', '0'), base=2)
+    col = int(seat.col.replace('R', '1').replace('L', '0'), base=2)
+    return row * 8 + col
 
 
-def col_to_int(row):
-    ret = 0
-    if row[0] == 'R':
-        ret += 4
-    if row[1] == 'R':
-        ret += 2
-    if row[2] == 'R':
-        ret += 1
-
-    return ret
-
-
-def get_seat_ids(seat_list):
-    return [row_to_int(seat.row) * 8 + col_to_int(seat.col) for seat in seat_list]
+def get_seat_ids(seats):
+    return [get_seat_id(seat) for seat in seats]
 
 
 if __name__ == '__main__':
-    seats = read_seats('d5in.txt')
-    seat_ids = get_seat_ids(seats)
+    all_seats = read_seats('d5in.txt')
+    seat_ids = get_seat_ids(all_seats)
 
-    print(max(seat_ids))
+    largest_seat_id = max(seat_ids)
+    print(largest_seat_id)
--- day05/d5b.py
+++ day05/d5b.py
@@ -1,12 +1,18 @@
 from day05.d5a import read_seats, get_seat_ids
 
-seats = read_seats('d5in.txt')
-seat_ids = sorted(get_seat_ids(seats))
 
-ix = 0
-while seat_ids[ix + 1] == seat_ids[ix] + 1:
-    ix += 1
+def find_my_seat(sids):
+    sorted_ids = sorted(sids)
+    for ix, sid in enumerate(sorted_ids):
+        if sorted_ids[ix + 1] != sid + 1:
+            return sid + 1
 
-print(seat_ids[ix] + 1)
+
+if __name__ == '__main__':
+    all_seats = read_seats('d5in.txt')
+    seat_ids = get_seat_ids(all_seats)
+
+    my_seat = find_my_seat(seat_ids)
+    print(my_seat)
 
 
