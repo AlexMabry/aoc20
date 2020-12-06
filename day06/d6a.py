dc07c20f1f9b6810d4a84b60ac27cf97a1cb73fe
Alex Mabry <alex.mabry@synapse-wireless.com>
2020-12-06 13:13:15 -0600

    Day 6

---
+++ day06/d6a.py
@@ -0,0 +1,17 @@
+lines = [n.strip() for n in open('d6in.txt').read().splitlines()]
+
+groups = []
+new_group = True
+
+for line in lines:
+    if new_group:
+        groups.append(set([c for c in line]))
+        new_group = False
+    elif line == '':
+        new_group = True
+    else:
+        groups[-1] |= set([c for c in line])
+
+counts = [len(group) for group in groups]
+
+print(sum(counts))
