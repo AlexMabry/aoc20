31992be699471be38f8f54a79c7a1d2fbc4d2306
Alex Mabry <alex.mabry@synapse-wireless.com>
2020-12-08 08:34:29 -0600

    Day 8

--- day08/d8a.py
+++ day08/d8a.py
@@ -1,1 +1,20 @@
-lines = [n.strip() for n in open('d8in.txt').read().splitlines()]
+lines = [n.strip().split(' ') for n in open('d8in.txt').read().splitlines()]
+
+past = set()
+commands = [(cmd, int(num)) for [cmd, num] in lines]
+
+acc = 0
+ptr = 0
+while ptr not in past:
+    past.add(ptr)
+    cmd, num = commands[ptr]
+
+    if cmd == 'acc':
+        acc += num
+
+    if cmd == 'jmp':
+        ptr += num
+    else:
+        ptr += 1
+
+print(acc)
--- day08/d8b.py
+++ day08/d8b.py
@@ -1,1 +1,30 @@
-lines = [n.strip() for n in open('d8in.txt').read().splitlines()]
+lines = [n.strip().split(' ') for n in open('d8in.txt').read().splitlines()]
+
+commands = [(cmd, int(num)) for [cmd, num] in lines]
+jmp_nop = iter([ix for ix, [cmd, num] in enumerate(commands) if cmd in ['nop', 'jmp']])
+
+acc = 0
+ptr = 0
+
+while not ptr >= len(commands):
+    ptr = 0
+    acc = 0
+    past = {len(commands)}
+    change = next(jmp_nop)
+
+    while ptr not in past:
+        past.add(ptr)
+        cmd, num = commands[ptr]
+
+        if cmd == 'acc':
+            acc += num
+
+        if ptr == change:
+            cmd = 'jmp' if cmd == 'nop' else 'nop'
+
+        if cmd == "jmp":
+            ptr += num
+        else:
+            ptr += 1
+
+print(acc)
