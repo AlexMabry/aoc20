d773df20d15ec9f455aaf0dbeb8ade0c24edcacd
Alex Mabry <alex.mabry@synapse-wireless.com>
2020-12-13 23:40:21 -0600

    Day 14

--- day14/d14a.py
+++ day14/d14a.py
@@ -1,1 +1,17 @@
-lines = [n.strip() for n in open('d14in.txt').read().splitlines()]
+import re
+
+lines = [n.strip().split(' = ') for n in open('d14in.txt').read().splitlines()]
+
+mask = ''
+locations = {}
+
+for [cmd, val] in lines:
+    if cmd == 'mask':
+        mask = val
+    else:
+        loc = int(re.match(r'mem\[([0-9]+)\]', cmd)[1])
+        val = format(int(val), 'b').zfill(36)
+        locations[loc] = int(''.join([m if m != 'X' else v for v, m in zip(val, mask)]), 2)
+
+print(sum(locations.values()))
+
--- day14/d14b.py
+++ day14/d14b.py
@@ -1,1 +1,26 @@
-lines = [n.strip() for n in open('d14in.txt').read().splitlines()]
+import re
+
+
+def replace(msk, num, dig):
+    for c in format(int(num), 'b').zfill(dig):
+        msk = msk.replace('X', c, 1)
+
+    return int(msk, 2)
+
+
+mask = ''
+locations = {}
+
+lines = [n.strip().split(' = ') for n in open('d14in.txt').read().splitlines()]
+for [cmd, val] in lines:
+    if cmd == 'mask':
+        mask = val
+    else:
+        loc = format(int(re.match(r'mem\[([0-9]+)\]', cmd)[1]), 'b').zfill(36)
+        result = ''.join([v if m == '0' else '1' if m == '1' else 'X' for v, m in zip(loc, mask)])
+        unknowns = result.count('X')
+        entries = [replace(result, x, unknowns) for x in range(2**unknowns)]
+        for e in entries:
+            locations[e] = int(val)
+
+print(sum(locations.values()))
