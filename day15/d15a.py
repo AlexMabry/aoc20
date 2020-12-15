707b9484e931fe8968ce2c8e2118d40c20107844
Alex Mabry <alex.mabry@synapse-wireless.com>
2020-12-15 14:26:45 -0600

    Day 15

--- day15/d15a.py
+++ day15/d15a.py
@@ -1,1 +1,15 @@
-lines = [n.strip() for n in open('d15in.txt').read().splitlines()]
+recent = {
+    9: (1, 1),
+    3: (2, 2),
+    1: (3, 3),
+    0: (4, 4),
+    8: (5, 5),
+    4: (6, 6)
+}
+
+last = 4
+for turn in range(len(recent.keys()) + 1, 2_021):
+    last = recent[last][1] - recent[last][0]
+    recent[last] = (recent[last][1] if last in recent else turn, turn)
+
+print(last)
