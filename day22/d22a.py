674d5fd1411176972d879f3a6921b2afc330ae2c
Alex Mabry <alex.mabry@synapse-wireless.com>
2020-12-22 19:44:12 -0600

    Day 22

--- day22/d22a.py
+++ day22/d22a.py
@@ -1,1 +1,17 @@
-lines = [n.strip() for n in open('d22in.txt').read().splitlines()]
+from collections import deque
+
+p1_deck = deque([int(n.strip()) for n in open('player1.txt').read().splitlines()])
+p2_deck = deque([int(n.strip()) for n in open('player2.txt').read().splitlines()])
+
+while p1_deck and p2_deck:
+    p1_card, p2_card = p1_deck.popleft(), p2_deck.popleft()
+
+    if p1_card > p2_card:
+        p1_deck.extend([p1_card, p2_card])
+    else:
+        p2_deck.extend([p2_card, p1_card])
+
+both_decks = [*p1_deck, *p2_deck]
+print(sum([card*(len(both_decks)-level) for level, card in enumerate(both_decks)]))
+
+
--- day22/d22b.py
+++ day22/d22b.py
@@ -1,1 +1,41 @@
-lines = [n.strip() for n in open('d22in.txt').read().splitlines()]
+from collections import deque
+from itertools import islice
+
+p1_deck = deque([int(n.strip()) for n in open('player1.txt').read().splitlines()])
+p2_deck = deque([int(n.strip()) for n in open('player2.txt').read().splitlines()])
+game_hands = {}
+
+def play_game(p1, p2):
+    beginning_hands = (tuple(p1), tuple(p2))
+
+    if beginning_hands not in game_hands:
+        round_hands = set()
+        while p1 and p2:
+            if (tuple(p1), tuple(p2)) in round_hands:
+                return "p1"
+            else:
+                round_hands.add((tuple(p1), tuple(p2)))
+
+            p1_card, p2_card = p1.popleft(), p2.popleft()
+
+            if len(p1) >= p1_card and len(p2) >= p2_card:
+                p1_sub_deck = deque([c for c in islice(p1, 0, p1_card)])
+                p2_sub_deck = deque([c for c in islice(p2, 0, p2_card)])
+                winner = play_game(p1_sub_deck, p2_sub_deck)
+            else:
+                winner = "p1" if p1_card > p2_card else "p2"
+
+            if winner == "p1":
+                p1.extend([p1_card, p2_card])
+            else:
+                p2.extend([p2_card, p1_card])
+
+        game_hands[beginning_hands] = "p1" if p1 else "p2"
+
+    return game_hands[beginning_hands]
+
+
+big_winner = play_game(p1_deck, p2_deck)
+both_decks = [*p1_deck, *p2_deck]
+print(sum([card*(len(both_decks)-level) for level, card in enumerate(both_decks)]))
+
